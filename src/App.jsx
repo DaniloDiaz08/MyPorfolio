f24b6db928b6dada9e8f932012fbb6608816279e
DaniloDiaz08 <danilodiaz340@gmail.com>
2023-04-06 13:36:32 -0500

    Aggregate contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/Home'
 import About from './Components/About'
 import Skills from './Components/Skills'
 import Work from './Components/Work'
+import Contact from './Components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <About/>
       <Skills/>
       <Work/>
+      <Contact/>
     </div>
   )
 }
